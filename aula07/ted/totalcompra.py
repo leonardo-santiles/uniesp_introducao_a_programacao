00cc6f06c5609334a54b98ffc1b1914b805fe0d5
leonardo.santiles <leonardo.santiles@evoluservices.com>
2022-09-25 12:29:45 -0300

    adiciona ted aula 07

---
+++ aula07/ted/totalcompra.py
@@ -0,0 +1,12 @@
+'''
+As maçãs custam R$ 1,30 cada se forem compradas menos de uma dúzia, e R$ 1,00
+se forem compradas pelo menos 12. Escreva um programa que leia o número de
+maçãs compradas, calcule e escreva o custo total da compra.
+'''
+
+num = int(input('Digite o número de maçãs compradas: '));
+
+if (num < 12):
+    print(f'Total da compra: R$ {(1.3 * num):.2f}');
+else:
+    print(f'Total da compra: R$ {num:.2f}');
